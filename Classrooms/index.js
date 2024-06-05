9ab7301b1b7fb45eb3d9c570670ffb7344225f1f
Usayd Ather <i200907@nu.edu.pk>
2024-06-05 14:36:34 +0500

    jenkinsfile

--- Classrooms/index.js
+++ Classrooms/index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
-var url = 'mongodb://localhost:27017/';
+var url = process.env.MONGO_URL || 'mongodb://localhost:27017/';
 
 app.post('/create_class', async (req, res) => {
     const classId = randomBytes(3).toString('hex');
